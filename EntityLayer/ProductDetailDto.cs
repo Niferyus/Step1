3de38461e4ddc72fb7d3cc30f90c132f8071eb22
Furkan Şahin Varışlı <varislifurkansahin@gmail.com>
2025-08-19 00:15:56 +0300

    Dtos added

--- EntityLayer/ProductDetailDto.cs
+++ EntityLayer/ProductDetailDto.cs
@@ -1,25 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EntityLayer
 {
-    public class Product
+    public class ProductDetailDto
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        [Required]
-        [Column(TypeName = "varchar(50)")]
         public string Name { get; set; }
-        [Required]
         public decimal Price { get; set; }
         public List<string> ImageUrl { get; set; }
-        [Column(TypeName = "varchar(150)")]
         public string Description { get; set; }
     }
 }
--- WepAPI/Program.cs
+++ WepAPI/Program.cs
@@ -1,3 +1,4 @@
+using DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
