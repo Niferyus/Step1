42bb3149aa4aaf75ecc22960b40272ad5e8f529a
Furkan Şahin Varışlı <varislifurkansahin@gmail.com>
2025-08-19 11:51:12 +0300

    Generic repository added

---
+++ DataAccessLayer/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly Context _context;
+        public GenericRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task Delete(int id)
+        {
+            _context.Remove(id);
+        }
+
+        public Task<List<T>> GetAll()
+        {
+           return _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);   
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
